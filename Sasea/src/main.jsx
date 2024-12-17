85a2835643a7472f5baa9264be688cbe98352dfd
tuan <minhtuankf@gmail.com>
2024-12-17 18:48:17 +0700

    add theme

--- Sasea/src/main.jsx
+++ Sasea/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.jsx'
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
