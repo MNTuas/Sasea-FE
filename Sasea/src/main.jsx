bf0d7060d4fb9c3811d35a1c871e177117b8c1b9
tuan <minhtuankf@gmail.com>
2024-12-18 15:35:45 +0700

    add theme light dark mode

--- Sasea/src/main.jsx
+++ Sasea/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.jsx'
-import { ThemeProvider } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>,
 )
--- Sasea/src/theme.js
+++ Sasea/src/theme.js
@@ -1,20 +1,24 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors';
+import { experimental_extendTheme as extendTheme} from '@mui/material/styles';
+
 
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    primary: {
-      main: red[500],
+const theme = extendTheme({
+    colorSchemes: {
+      light: {
+        palette: {
+          // primary: {
+          //   main: '#ff5252',
+          // },
+        },
+      },
+      dark: {
+        palette: {
+          // primary: {
+          //   main: '#000',
+          // },
+        },
+      },
     },
-    secondary: {
-      main: '#556cd6',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
-});
+  });
 
 export default theme;
