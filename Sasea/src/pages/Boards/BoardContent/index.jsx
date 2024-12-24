aa88f42b01cbcf91ddc26c2dd42253fd9e1ea855
tuan <minhtuankf@gmail.com>
2024-12-24 18:09:17 +0700

    board content

--- Sasea/src/pages/Boards/BoardContent/index.jsx
+++ Sasea/src/pages/Boards/BoardContent/index.jsx
@@ -1,4 +1,8 @@
 import { Box } from "@mui/material"
+import theme from "~/theme"
+
+const COLUMN_HEADER_HEIGHT = '50px'
+const COLUMN_FOOTER_HEIGHT = '56px'
 
 function BoardContent(){
     return(
@@ -6,10 +10,23 @@ function BoardContent(){
             backgroundColor:'primary.main',
             width:'100%',
             height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-            display: 'flex',
-            alignItems: 'center'
+            display: 'flex',  
           }}>
-            Content
+            {/* Box column */}
+            <Box sx={{
+              minWidth: '300px',
+              maxWidth: '300px',
+              bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#333643' : '#ebecf0'),
+              ml: 2,
+              borderRadius: '6px',
+
+            }}>
+              <Box sx={{}}>Header</Box>
+
+              <Box sx={{}}>List Card</Box>
+              
+              <Box sx={{}}>Footer</Box>
+            </Box>
           </Box>
     )
 }
